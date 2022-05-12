b5b31ec6a20b2a610b53e2a1d88a8f5504687731
Ivan Higa <ivzeq.higa@gmail.com>
2022-05-12 01:57:19 -0300

    Project v1 - Display, add/remove, toggle read/unread

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-}
-
-Book.prototype.info = function(){
-    return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.read;
+    this.info = ()=>{return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.read;}
+    this.readBook = ()=>{this.read == 'read'? this.read = 'not read': this.read = 'read';}
 }
 
 function addBookToLibrary(book){
@@ -16,8 +14,7 @@ function addBookToLibrary(book){
 }
 
 function displayBooksInLibrary(){
-    const bookDisplayChilds = document.querySelectorAll('#book-display *')
-    bookDisplay.remove(bookDisplayChilds)
+    bookDisplay.innerHTML = "";
     myLibrary.forEach(book =>{
         const bookCard= document.createElement('div');
         bookCard.classList.add('book-card');
@@ -27,25 +24,64 @@ function displayBooksInLibrary(){
         bookCard.appendChild(bookInfoList)
 
         for (const key in book) {
-            if (key != 'info') {
+            if (key != 'info' && key != 'readBook') {
                 const bookInfoLine = document.createElement('li')
                 bookInfoLine.textContent = key + ': ' + book[key];
                 bookInfoList.appendChild(bookInfoLine)
             }
             
         }
+        
+        const bookButtonContainer = document.createElement('div');
+        bookButtonContainer.classList.add('remove-button-container');
+        bookCard.appendChild(bookButtonContainer)
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = "Remove";
+        removeButton.classList.add('remove-button');
+        removeButton.addEventListener('click', ()=>{
+            myLibrary.splice(myLibrary.indexOf(book),1)
+            displayBooksInLibrary();
+        })
+        
+        const readUnreadButton = document.createElement('button');
+        readUnreadButton.textContent = "Read/unread";
+        readUnreadButton.classList.add('read-unread-button');
+        readUnreadButton.addEventListener('click', ()=>{
+            book.readBook();
+            displayBooksInLibrary();
+        })
+
+        bookButtonContainer.appendChild(removeButton)
+        bookButtonContainer.appendChild(readUnreadButton)
 
         bookDisplay.appendChild(bookCard)
     })
 }
 
+
+
 const newBookButton = document.querySelector('#new-book')
+const displayButton = document.querySelector('#display-books')
+const addBookButton = document.querySelector('#add-book')
 const bookForm = document.querySelector('#form-container')
 const bookDisplay = document.querySelector('#book-display')
+
+const formTitle = document.querySelector('#form-title')
+const formAuthor = document.querySelector('#form-author')
+const formPages = document.querySelector('#form-pages')
+const formRead = document.querySelector('#form-read')
 
 newBookButton.addEventListener('click', () =>{
     bookForm.classList.remove('invisible');
 })
+
+addBookButton.addEventListener('click', ()=>{
+    addBookToLibrary(new Book(formTitle.value, formAuthor.value, formPages.value, formRead.value));
+    displayBooksInLibrary();
+})
+
+displayButton.addEventListener('click', displayBooksInLibrary);
 
 const book1 = new Book('Early Years', 'TurtleMe', 193, 'read');
 const book2 = new Book('New Heights', 'TurtleMe', 202, 'read');
@@ -53,4 +89,3 @@ const book3 = new Book('A Game of Thrones', 'George R. R. Martin', 694, 'not rea
 
 addBookToLibrary(book1)
 addBookToLibrary(book3)
-displayBooksInLibrary();
